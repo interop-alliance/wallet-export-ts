ec2a868e950932223e790539aea70c4f79484d78
Omar Salah <omar.salah1597@gmail.com>
2024-11-03 21:40:26 +0200

    add impioty fn to build script

--- build-dist.sh
+++ build-dist.sh
@@ -2,6 +2,7 @@ mkdir ./dist/esm
 cat >dist/esm/index.js <<!EOF
 import cjsModule from "../index.js";
 export const exportActorProfile = cjsModule.exportActorProfile;
+export const importActorProfile = cjsModule.importActorProfile;
 !EOF
 
 cat >dist/esm/package.json <<!EOF
