910b8fff86f79a05eb6cc95c6341834fa370546c
Omar Salah <omar.salah1597@gmail.com>
2025-01-09 16:32:28 +0200

    Add validateExportStream to ESM build script

--- build-dist.sh
+++ build-dist.sh
@@ -1,8 +1,9 @@
-mkdir ./dist/esm
+mkdir -p ./dist/esm
 cat >dist/esm/index.js <<!EOF
 import cjsModule from "../index.js";
 export const exportActorProfile = cjsModule.exportActorProfile;
 export const importActorProfile = cjsModule.importActorProfile;
+export const validateExportStream = cjsModule.validateExportStream;
 !EOF
 
 cat >dist/esm/package.json <<!EOF
